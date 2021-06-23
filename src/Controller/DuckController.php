b70b93ab884f4df4b6f6dcbf0f17a305ecc1b38c
Lauric-h <h.lauric@outlook.com>
2021-06-23 10:21:47 +0200

    fin etape 2: voter,publication, view profile

---
+++ src/Controller/DuckController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ducks;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DuckController extends AbstractController
+{
+    /**
+     * @Route("/duck", name="duck")
+     */
+    public function index(): Response
+    {
+        return $this->render('duck/index.html.twig', [
+            'controller_name' => 'DuckController',
+        ]);
+    }
+
+    /**
+     * @Route("/duck/{id}", name="show_duck", methods={"GET"})
+     */
+    public function show(Ducks $ducks): Response
+    {
+        return $this->render('ducks/show.html.twig', [
+            'ducks' => $ducks
+        ]);
+    }
+}
